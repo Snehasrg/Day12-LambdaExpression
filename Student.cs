567c321804049f48352c3d58539ad5beaecb4bd9
Snehasrg <sghongade642@gmail.com>
2023-04-07 23:35:54 +0530

    [sneha]Added Program for displaying all student details

---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+        public override string ToString()
+        {
+            return $"id:{Id} name:{Name} age:{Age} address:{Address} marks:{TotalMarks}";
+
+        }
+    }
+}
